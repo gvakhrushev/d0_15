130128807f965b1730d6dec5314ad8ca28c9d175
Grigori Vakhrushev <g.vakhrushev1993@gmail.com>
2026-09-19 21:05:37 +0300

    build: include concrete sedenion tower

--- 03_FORMALIZATION/D0/All.lean
+++ 03_FORMALIZATION/D0/All.lean
@@ -12,6 +12,7 @@ import D0.Algebra.FibonacciPerronTraceCanonicity
 import D0.Algebra.GaloisComposite
 import D0.Algebra.GaloisPhiNontrivial
 import D0.Algebra.HurwitzLocalBoundary
+import D0.Algebra.SedenionTower
 import D0.Algebra.Sedenions
 import D0.Born
 import D0.Bridge.AdlerWeissBridge
