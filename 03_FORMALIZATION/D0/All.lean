5e3d580314ad5d35d27e3cf54692793c17f77156
Grigori Vakhrushev <g.vakhrushev1993@gmail.com>
2026-09-19 22:06:54 +0300

    build: restore generated import order

--- 03_FORMALIZATION/D0/All.lean
+++ 03_FORMALIZATION/D0/All.lean
@@ -114,9 +114,9 @@ import D0.Cosmology.EntropyArchiveFlow
 import D0.Cosmology.EntropyJacobianSign
 import D0.Cosmology.FeedbackPartitionFunction
 import D0.Cosmology.FiedlerHodgeProjection
-import D0.Cosmology.FiedlerTiltTautologyNoGo
 import D0.Cosmology.FiedlerProjectorOperator
 import D0.Cosmology.FiedlerRelaxationLimit
+import D0.Cosmology.FiedlerTiltTautologyNoGo
 import D0.Cosmology.FiniteFeedbackEquationOfState
 import D0.Cosmology.FloorMassProjection
 import D0.Cosmology.HeatTraceEnergyFunctional
