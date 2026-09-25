4b9aedddf109f2bb529108d70e00f28dd1e558b5
Grigori Vakhrushev <g.vakhrushev1993@gmail.com>
2026-09-25 15:18:38 +0300

    Wire relative A/e graphification closure into D0.All

--- 03_FORMALIZATION/D0/All.lean
+++ 03_FORMALIZATION/D0/All.lean
@@ -312,6 +312,9 @@ import D0.Geometry.A4DReferenceJunctionCompressionBoundary
 import D0.Geometry.A4DReferenceLocalCandidateNoGo
 import D0.Geometry.A4DRegularAEPassport
 import D0.Geometry.A4DRelativeAEComparisonSpan
+import D0.Geometry.A4DRelativeAEGraphificationClosureTwoSided
+import D0.Geometry.A4DRelativeAEGraphificationClosureRank
+import D0.Geometry.A4DRelativeAEGraphificationClosure
 import D0.Geometry.A4DRoleOverlapTwistedCocycle
 import D0.Geometry.A4DRolePairMetricCarrier
 import D0.Geometry.A4DRoleSpatialRepresentationWeld
