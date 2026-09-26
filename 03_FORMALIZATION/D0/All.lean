d888c0e0c35b9c348530ca2729ebb7f1ef73fb04
Grigori Vakhrushev <g.vakhrushev1993@gmail.com>
2026-09-26 15:12:59 +0300

    recover: import star Einstein seed

--- 03_FORMALIZATION/D0/All.lean
+++ 03_FORMALIZATION/D0/All.lean
@@ -332,6 +332,7 @@ import D0.Geometry.A4DSolderMetricCompletion
 import D0.Geometry.A4DSourcedMismatchFactorPassport
 import D0.Geometry.A4DStaggeredFirstJetPathExpansion
 import D0.Geometry.A4DStaggeredHodgeSelector
+import D0.Geometry.A4DStarEinsteinSeed
 import D0.Geometry.A4DStarFiniteLorentzQuotient
 import D0.Geometry.A4DStarTranslationCartanHodgeNoGo
 import D0.Geometry.A4DSymRoleCentralDifference
