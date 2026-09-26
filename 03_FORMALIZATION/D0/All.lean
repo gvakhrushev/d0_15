54c4a51a59658bd0d9309c64c37057881953009d
Grigori Vakhrushev <g.vakhrushev1993@gmail.com>
2026-09-26 12:03:52 +0300

    worker: include finite Lorentz quotient core

--- 03_FORMALIZATION/D0/All.lean
+++ 03_FORMALIZATION/D0/All.lean
@@ -331,6 +331,7 @@ import D0.Geometry.A4DSolderMetricCompletion
 import D0.Geometry.A4DSourcedMismatchFactorPassport
 import D0.Geometry.A4DStaggeredFirstJetPathExpansion
 import D0.Geometry.A4DStaggeredHodgeSelector
+import D0.Geometry.A4DStarFiniteLorentzQuotient
 import D0.Geometry.A4DStarTranslationCartanHodgeNoGo
 import D0.Geometry.A4DSymRoleCentralDifference
 import D0.Geometry.A4DTransportedReferenceMismatch
