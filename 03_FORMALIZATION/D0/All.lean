192d28e6ef719ebf83cc7856b4357d661ae5111f
Grigori Vakhrushev <g.vakhrushev1993@gmail.com>
2026-09-26 15:43:24 +0300

    recover: import gauge-image seam resolution

--- 03_FORMALIZATION/D0/All.lean
+++ 03_FORMALIZATION/D0/All.lean
@@ -289,6 +289,7 @@ import D0.Geometry.A4DCrossedDerivationIntegrability
 import D0.Geometry.A4DCrossedPathAlgebraBoundary
 import D0.Geometry.A4DDiscreteEnergyKernel
 import D0.Geometry.A4DGoldenCarrierWeldBoundary
+import D0.Geometry.A4DGaugeImageResolution
 import D0.Geometry.A4DGoldenRolePhaseRGDefect
 import D0.Geometry.A4DHorizontalDressingSecondJetFreedom
 import D0.Geometry.A4DJointHolonomyResidual
